e9f9de2bebe39d902ed3f6223965200f14bd7c83
Annabelle Richard <annabelle.e.richard@gmail.com>
2015-01-27 20:04:29 -0800

    Initial commit of files for problem 2.

---
+++ InterpFun.hs
@@ -0,0 +1,36 @@
+module InterpFun where
+
+import Expr
+import Result
+
+-- Values resulting from interpreting an expression.
+data Val = NumV Integer
+         | BoolV Bool
+         | FunV Var CExpr Env
+         | PrimV String (Val -> Result Val)  -- name and implementation
+
+type Env = [(Var, Val)]
+
+instance Show Val where
+  show (NumV n) = show n
+  show (BoolV b) = show b
+  show (FunV var body env) = "(fun (" ++ var ++ ") " ++ (show body) ++ " | " ++
+                             (show env) ++ ")"
+  show (PrimV name impl) = "<primitive: " ++ name ++ ">"
+
+wrapBinaryArithOp :: String -> (Integer -> Integer -> Val) -> Val
+wrapBinaryArithOp name op =
+  PrimV name (
+    \arg1 -> return (PrimV ("partial:" ++ name)
+                     (\arg2 ->
+                       case (arg1, arg2) of
+                        (NumV lv, NumV rv) -> return (op lv rv)
+                        nonNum -> fail ("numeric op applied to: " ++
+                                        (show nonNum)))))
+
+-- Populate initialEnv ...
+initialEnv :: Env
+initialEnv = []
+
+interp :: CExpr -> Env -> Result Val
+interp expr env = Err "'interp' not yet implemented"
